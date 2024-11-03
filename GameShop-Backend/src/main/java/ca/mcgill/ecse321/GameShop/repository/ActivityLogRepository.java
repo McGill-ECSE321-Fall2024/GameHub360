9a75e005d11696c8c7e585781b5d61c3cba56a75
Ahmed Mossa <ahmed.mossa@mail.mcgill.ca>
2024-11-03 01:09:21 -0500

    Added all required files, implemented the endpoints and controllers, added the logging features for account-realted activities. TBD: Tests

--- GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/repository/ActivityLogRepository.java
+++ GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/repository/ActivityLogRepository.java
@@ -1,5 +1,7 @@
 package ca.mcgill.ecse321.GameShop.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.GameShop.model.ActivityLog;
@@ -7,4 +9,7 @@ import ca.mcgill.ecse321.GameShop.model.ActivityLog;
 public interface ActivityLogRepository extends CrudRepository<ActivityLog, Integer> {
     // Find activity log by log id
     public ActivityLog findActivityLogByLogId(Integer logId);
+
+    // Find activity log by employee id
+    public List<ActivityLog> findActivityLogByEmployeeId(Integer employeeId);
 }
