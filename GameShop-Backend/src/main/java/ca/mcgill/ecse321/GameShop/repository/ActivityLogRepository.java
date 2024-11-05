98db23fe6941cba2abb8612b9dcd5adcf0fca263
Ahmed Mossa <ahmed.mossa@mail.mcgill.ca>
2024-11-05 12:52:49 -0500

    Fixing The /{employeeId}/activities Endpoint to have a successful build.

--- GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/repository/ActivityLogRepository.java
+++ GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/repository/ActivityLogRepository.java
@@ -1,7 +1,5 @@
 package ca.mcgill.ecse321.GameShop.repository;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.GameShop.model.ActivityLog;
@@ -9,7 +7,4 @@ import ca.mcgill.ecse321.GameShop.model.ActivityLog;
 public interface ActivityLogRepository extends CrudRepository<ActivityLog, Integer> {
     // Find activity log by log id
     public ActivityLog findActivityLogByLogId(Integer logId);
-
-    // Find activity log by employee id
-    public List<ActivityLog> findActivityLogByEmployeeId(Integer employeeId);
 }
