984ea8f1e61b17e0b0c95651ff96314cbe168cba
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2024-11-07 16:32:03 -0500

    finish store info service

--- GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/dto/SalesMetricsDto.java
+++ GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/dto/SalesMetricsDto.java
@@ -1,5 +1,53 @@
 package ca.mcgill.ecse321.GameShop.dto;
 
 public class SalesMetricsDto {
+    // Attributes
+    private double totalSales;
+    private int totalOrders;
+    private int totalGamesSold;
+    private int totalCustomers;
 
+    // Constructors
+    public SalesMetricsDto() {
+    }
+
+    public SalesMetricsDto(double totalSales, int totalOrders, int totalGamesSold, int totalCustomers) {
+        this.totalSales = totalSales;
+        this.totalOrders = totalOrders;
+        this.totalGamesSold = totalGamesSold;
+        this.totalCustomers = totalCustomers;
+    }
+
+    // Getters and Setters
+    public double getTotalSales() {
+        return totalSales;
+    }
+
+    public void setTotalSales(double totalSales) {
+        this.totalSales = totalSales;
+    }
+
+    public int getTotalOrders() {
+        return totalOrders;
+    }
+
+    public void setTotalOrders(int totalOrders) {
+        this.totalOrders = totalOrders;
+    }
+
+    public int getTotalGamesSold() {
+        return totalGamesSold;
+    }
+
+    public void setTotalGamesSold(int totalGamesSold) {
+        this.totalGamesSold = totalGamesSold;
+    }
+
+    public int getTotalCustomers() {
+        return totalCustomers;
+    }
+
+    public void setTotalCustomers(int totalCustomers) {
+        this.totalCustomers = totalCustomers;
+    }
 }
--- GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/dto/StoreInformationRequestDto.java
+++ GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/dto/StoreInformationRequestDto.java
@@ -1,7 +1,10 @@
 package ca.mcgill.ecse321.GameShop.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class StoreInformationRequestDto {
     // Attributes
+    @NotBlank(message = "Store policy cannot be empty.")
     private String storePolicy;
 
     // Constructors
