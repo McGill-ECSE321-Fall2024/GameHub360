3da3e72e621f2d65ad902424a9ee9a078e7a782e
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2024-11-07 18:51:59 -0500

    fix promo list DTO

--- GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/dto/PromotionListDto.java
+++ GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/dto/PromotionListDto.java
@@ -3,12 +3,18 @@ package ca.mcgill.ecse321.GameShop.dto;
 import java.util.List;
 
 public class PromotionListDto {
+    // Attributes
     private List<PromotionResponseDto> promotions;
+
+    // Constructors
+    public PromotionListDto() {
+    }
 
     public PromotionListDto(List<PromotionResponseDto> promotions) {
         this.promotions = promotions;
     }
 
+    // Getters and Setters
     public List<PromotionResponseDto> getPromotions() {
         return promotions;
     }
