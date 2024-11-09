7522e64848eb9147633e38a77ab7de4187a4073c
Ahmed Mossa <ahmed.mossa@mail.mcgill.ca>
2024-11-08 22:16:30 -0500

    Fixed the the exceptions

--- GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/exception/GameShopExceptionHandler.java
+++ GameShop-Backend/src/main/java/ca/mcgill/ecse321/GameShop/exception/GameShopExceptionHandler.java
@@ -1,16 +1,15 @@
 package ca.mcgill.ecse321.GameShop.exception;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import ca.mcgill.ecse321.GameShop.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.validation.FieldError;
 
-import ca.mcgill.ecse321.GameShop.dto.ErrorResponseDto;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GameShopExceptionHandler {
