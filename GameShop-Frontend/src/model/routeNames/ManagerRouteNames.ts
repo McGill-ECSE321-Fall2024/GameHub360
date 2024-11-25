0f2f5f029c42ddb36a842c11686fe95b1d6e481f
Ahmed Mossa <ahmed.mossa@mail.mcgill.ca>
2024-11-25 00:42:55 -0500

    Fixed routes names

--- GameShop-Frontend/src/model/routeNames/ManagerRouteNames.ts
+++ GameShop-Frontend/src/model/routeNames/ManagerRouteNames.ts
@@ -1,12 +1,12 @@
 export enum ManagerRouteNames {
-  DASHBOARD = 'manager/dashboard',
-  GAME_REQUESTS = 'manager/game-requests',
-  GAME_REQUEST_DETAIL = 'manager/game-requests/:id', //*:id is a placeholder for the game request ID
-  GAMES = 'manager/games',
-  CREATE_GAME = 'manager/games/create',
-  GAME_DETAIL = 'manager/games/:id',
-  CATEGORIES = 'manager/categories',
-  CREATE_CATEGORY = 'manager/categories/create',
-  CATEGORY_DETAIL = 'manager/categories/:id',
+  DASHBOARD = '/manager/dashboard',
+  GAME_REQUESTS = '/manager/game-requests',
+  GAME_REQUEST_DETAIL = '/manager/game-requests/:id', //*:id is a placeholder for the game request ID
+  GAMES = '/manager/games',
+  CREATE_GAME = '/manager/games/create',
+  GAME_DETAIL = '/manager/games/:id',
+  CATEGORIES = '/manager/categories',
+  CREATE_CATEGORY = '/manager/categories/create',
+  CATEGORY_DETAIL = '/manager/categories/:id',
 }
 //* will use <Link to={`${ManagerRouteNames.GAME_REQUEST_DETAIL.replace(':id', request.id)}`}> title or whatever </Link>
