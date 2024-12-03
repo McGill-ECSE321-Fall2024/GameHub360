3a8fe6af1365f91dbda320272966d0ef0edddbf1
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2024-12-02 20:40:32 -0500

    add landing page

--- GameShop-Frontend/src/router/AuthRouter.tsx
+++ GameShop-Frontend/src/router/AuthRouter.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { GeneralRouteNames } from '../model/routeNames/GeneralRouteNames';
 import LoginPage from '../pages/auth/LoginPage';
 import SignupPage from '../pages/auth/SignupPage';
@@ -6,22 +6,21 @@ import BrowsePage from '../pages/BrowsePage';
 import Navbar from '../components/Navbar';
 import NotFoundPage from '../pages/NotFoundPage';
 import GameDetailsPage from '../pages/GameDetailsPage';
+import LandingPage from '../pages/LandingPage';
 
 const AuthRouter = () => {
   return (
     <>
       <Navbar userType={null} />
       <Routes>
+        <Route path={GeneralRouteNames.BASE} element={<LandingPage />} />
         <Route path={GeneralRouteNames.BROWSE} element={<BrowsePage />} />
-        <Route path={`${GeneralRouteNames.BROWSE}/game/:id`} element={<GameDetailsPage />} />
+        <Route
+          path={`${GeneralRouteNames.BROWSE}/game/:id`}
+          element={<GameDetailsPage />}
+        />
         <Route path={GeneralRouteNames.LOGIN} element={<LoginPage />} />
         <Route path={GeneralRouteNames.SIGNUP} element={<SignupPage />} />
-
-        {/* Redirect base route to browse */}
-        <Route
-          path={GeneralRouteNames.BASE}
-          element={<Navigate to={GeneralRouteNames.BROWSE} replace />}
-        />
 
         {/* 404 Page */}
         <Route path="*" element={<NotFoundPage />} />
